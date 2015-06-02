f63cd6a50a7f495fb4056cbef720044a269770bb
Daniel Lovasko <lovasko@freebsd.org>
2015-06-02 20:21:53 +0100

    print IDs instead of type names

--- src/functions.c
+++ src/functions.c
@@ -5,16 +5,15 @@
 static void
 dump_argument(void* argument, void* arg)
 {
-	ctf_type argument_type;
-	char* argument_type_name;
+	ctf_type type;
+	ctf_id id;
 
 	(void) arg;
 
-	ctf_argument_get_type(argument, &argument_type);
-	ctf_type_to_string(argument_type, &argument_type_name);
+	ctf_argument_get_type(argument, &type);
+	ctf_type_get_id(type, &id);
 
-	printf("     %s\n", argument_type_name);
-	free(argument_type_name);
+	printf("%d ", id);
 }
 
 static void
@@ -22,20 +21,20 @@ dump_function(void* function, void* arg)
 {
 	char* name;
 	ctf_type return_type;
-	char* return_type_name;
+	ctf_id return_id;
 
 	(void) arg;
 
 	ctf_function_get_name(function, &name);
 	ctf_function_get_return_type(function, &return_type);
-	ctf_type_to_string(return_type, &return_type_name);
+	ctf_type_get_id(return_type, &return_id);
 
-	printf("   Name: %s\n", name);
-	printf("Returns: %s\n", return_type_name);
-	free(return_type_name);
-
+	printf("     Name: %s\n", name);
+	printf("   Return: %d\n", return_id);
+	printf("Arguments: ");
 	ctf_function_foreach_argument(function, NULL, dump_argument);
-	printf("\n");
+
+	printf("\n\n");
 }
 
 void
