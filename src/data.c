f0ddf65eab187d684ac8f4f6dcacf0d1e5da0ada
Daniel Lovasko <lovasko@freebsd.org>
2015-06-02 20:21:42 +0100

    print IDs instead of type names

--- src/data.c
+++ src/data.c
@@ -7,20 +7,19 @@ static void
 dump_data_object(void* data_object, void* data)
 {
 	ctf_type type;
+	ctf_id id;
 	char* symbol_name;
-	char* type_name;
 
 	(void) data;
 
 	ctf_data_object_get_name(data_object, &symbol_name);
 	ctf_data_object_get_type(data_object, &type);
-	ctf_type_to_string(type, &type_name);
+	ctf_type_get_id(type, &id);
 
 	printf(" Name: %s\n", symbol_name);
-	printf(" Type: %s\n", type_name);
+	printf("   ID: %d\n", id);
 	printf("\n");
 
-	/* free(type_name); */
 }
 
 void
