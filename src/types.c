2cbc1dd78d2c56223ac6d4df7f9850c7e3631cec
Daniel Lovasko <lovasko@freebsd.org>
2015-06-02 20:22:00 +0100

    print IDs instead of type names

--- src/types.c
+++ src/types.c
@@ -23,11 +23,11 @@ dump_int(ctf_type type, void* arg)
 	ctf_int_get_content(_int, &content);
 	ctf_int_get_is_signed(_int, &is_signed);
 
-	printf("     Name: %s\n", name);
-	printf("     Size: %d\n", size);
-	printf("   Offset: %d\n", offset);
-	printf("   Signed: %s\n", is_signed ? "yes" : "no");
-	printf("  Content: %s\n", ctf_int_content_to_string(content));
+	printf("        Name: %s\n", name);
+	printf("        Size: %d\n", size);
+	printf("      Offset: %d\n", offset);
+	printf("      Signed: %s\n", is_signed ? "yes" : "no");
+	printf("     Content: %s\n", ctf_int_content_to_string(content));
 
 	return CTF_OK;
 }
@@ -49,10 +49,10 @@ dump_float(ctf_type type, void* arg)
 	ctf_float_get_offset(_float, &offset);
 	ctf_float_get_encoding(_float, &encoding);
 
-	printf("     Name: %s\n", name);
-	printf("     Size: %d\n", size);
-	printf("   Offset: %d\n", offset);
-	printf("  Content: %s\n", ctf_float_encoding_to_string(encoding));
+	printf("        Name: %s\n", name);
+	printf("        Size: %d\n", size);
+	printf("      Offset: %d\n", offset);
+	printf("     Content: %s\n", ctf_float_encoding_to_string(encoding));
 
 	return CTF_OK;
 }
@@ -61,21 +61,20 @@ static int
 dump_array(ctf_type type, void* arg)
 {
 	ctf_array array;
-	char* type_string;
 	ctf_type content_type;
+	ctf_id content_id;
 	ctf_array_length length;
 
 	(void) arg;
 
 	ctf_array_init(type, &array);
 	ctf_array_get_content_type(array, &content_type);
+	ctf_type_get_id(content_type, &content_id);
 	ctf_array_get_length(array, &length);
-	ctf_type_to_string(content_type, &type_string);
 
-	printf("  Content: %s\n", type_string);
-	printf("   Length: %d\n", length);
+	printf(" Content ID: %d\n", content_id);
+	printf("     Length: %d\n", length);
 
-	free(type_string);
 	return CTF_OK;
 }
 
@@ -84,20 +83,18 @@ dump_typedef(ctf_type type, void* arg)
 {
 	ctf_typedef _typedef;
 	char* name;
-	char* type_string;
-	ctf_type old_type;
+	ctf_type ref_type;
+	ctf_id ref_id;
 
 	(void) arg;
 
 	ctf_typedef_init(type, &_typedef);
 	ctf_typedef_get_name(_typedef, &name);
-	ctf_typedef_get_type(_typedef, &old_type);
-	ctf_type_to_string(old_type, &type_string);
+	ctf_typedef_get_type(_typedef, &ref_type);
+	ctf_type_get_id(ref_type, &ref_id);
 
-	printf(" Old type: %s\n", type_string);
-	printf(" New type: %s\n", name);
-
-	free(type_string);
+	printf("Reference ID: %d\n", ref_id);
+	printf("    New name: %s\n", name);
 	return CTF_OK;
 }
 
@@ -105,20 +102,18 @@ static void
 dump_member(void* member, void* arg)
 {
 	char* member_name;
-	char* member_type_string;
 	ctf_member_offset member_offset;
 	ctf_type member_type;
+	ctf_id member_id;
 
 	(void) arg;
 
 	ctf_member_get_name(member, &member_name);
-	ctf_member_get_type(member, &member_type);
 	ctf_member_get_offset(member, &member_offset);
-	ctf_type_to_string(member_type, &member_type_string);
+	ctf_member_get_type(member, &member_type);
+	ctf_type_get_id(member_type, &member_id);
 
-	printf("       %s %s | %llu\n", member_type_string, member_name, member_offset);
-
-	free(member_type_string);
+	printf("          %d | %s | %llu\n", member_id, member_name, member_offset);
 }
 
 static int 
@@ -133,11 +128,11 @@ dump_struct_union(ctf_type type, void* arg)
 	ctf_struct_union_init(type, &struct_union);
 	ctf_struct_union_get_name(struct_union, &name);
 
-	printf("     Name: %s\n", name);
+	printf("        Name: %s\n", name);
 
 	ctf_struct_union_get_member_count(struct_union, &member_count);
 	if (member_count == 0)
-		printf("    No members.");
+		printf("       No members.");
 	else
 		ctf_struct_union_foreach_member(struct_union, NULL, dump_member);
 
@@ -155,7 +150,7 @@ dump_enum_entry(void* enum_entry, void* arg)
 	ctf_enum_entry_get_name(enum_entry, &entry_name);
 	ctf_enum_entry_get_value(enum_entry, &entry_value);
 
-	printf("       %s = %d\n", entry_name, entry_value); 
+	printf("          %s = %d\n", entry_name, entry_value); 
 }
 
 static int 
@@ -170,11 +165,11 @@ dump_enum(ctf_type type, void* arg)
 	ctf_enum_init(type, &_enum);
 	ctf_enum_get_name(_enum, &name);
 
-	printf("     Name: %s\n", name);
+	printf("        Name: %s\n", name);
 
 	ctf_enum_get_entry_count(_enum, &entry_count);
 	if (entry_count == 0)
-		printf("    No entries.");
+		printf("       No entries.");
 	else
 		ctf_enum_foreach_enum_entry(_enum, NULL, dump_enum_entry);
 
@@ -192,7 +187,7 @@ dump_fwd_decl(ctf_type type, void* arg)
 	ctf_fwd_decl_init(type, &fwd_decl);
 	ctf_fwd_decl_get_name(fwd_decl, &name);
 
-	printf("     Name: %s\n", name);
+	printf("        Name: %s\n", name);
 	return CTF_OK;
 }
 
@@ -201,17 +196,15 @@ dump_pointer(ctf_type type, void* arg)
 {
 	ctf_pointer pointer;
 	ctf_type ref_type;
-	char* type_string;
+	ctf_id ref_id;
 
 	(void) arg;
 
 	ctf_pointer_init(type, &pointer);
 	ctf_pointer_get_type(pointer, &ref_type);
-	ctf_type_to_string(ref_type, &type_string);
+	ctf_type_get_id(ref_type, &ref_id);
 
-	printf("Reference: %s\n", type_string);
-
-	free(type_string);
+	printf("Reference ID: %d\n", ref_id);
 	return CTF_OK;
 }
 
@@ -228,15 +221,14 @@ static int
 dump_qualifier(ctf_type type, void* arg)
 {
 	ctf_type ref_type;
-	char* type_string;
+	ctf_id ref_id;
 
 	(void) arg;
 
 	ctf_type_init(type, &ref_type);
-	ctf_type_to_string(ref_type, &type_string);
+	ctf_type_get_id(ref_type, &ref_id);
 
-	printf("Reference: %s\n", type_string);
-	free(type_string);
+	printf("Reference ID: %d\n", ref_id);
 
 	return CTF_OK;
 }
@@ -244,38 +236,35 @@ dump_qualifier(ctf_type type, void* arg)
 static void
 dump_argument(void* argument, void* arg)
 {
-	ctf_type argument_type;
-	char* argument_type_name;
+	ctf_type type;
+	ctf_id id;
 
 	(void) arg;
 
-	ctf_argument_get_type(argument, &argument_type);
-	ctf_type_to_string(argument_type, &argument_type_name);
+	ctf_argument_get_type(argument, &type);
+	ctf_type_get_id(type, &id);
 
-	printf("     %s\n", argument_type_name);
-	free(argument_type_name);
+	printf("%d ", id);
 }
 
 static int 
 dump_function(ctf_type type, void* arg)
 {
-	char* name;
 	ctf_type return_type;
-	char* return_type_name;
+	ctf_id return_id;
 	ctf_function function;
 
 	(void) arg;
 
 	ctf_function_init(type, &function);
-	ctf_function_get_name(function, &name);
 	ctf_function_get_return_type(function, &return_type);
-	ctf_type_to_string(return_type, &return_type_name);
+	ctf_type_get_id(return_type, &return_id);
 
-	printf("   Name: %s\n", name);
-	printf("Returns: %s\n", return_type_name);
-	free(return_type_name);
-
+	printf("      Return: %d\n", return_id);
+	printf("   Arguments: ");
 	ctf_function_foreach_argument(function, NULL, dump_argument);
+	printf("\n");
+
 	return CTF_OK;
 }
 
@@ -308,9 +297,9 @@ dump_type(void* type, void* data)
 	ctf_type_get_kind(type, &kind);
 	ctf_type_get_is_root(type, &is_root);
 
-	printf("       ID: %d\n", id);	
-	printf("     Kind: %s\n", ctf_kind_to_string(kind));
-	printf("     Root: %s\n", is_root ? "yes" : "no");
+	printf("          ID: %d\n", id);	
+	printf("        Kind: %s\n", ctf_kind_to_string(kind));
+	printf("        Root: %s\n", is_root ? "yes" : "no");
 		
 	ctf_type_polycall(type, NULL, dump_fns);
 	printf("\n");
